3a2568d833f38df9f95a3f59b67517c16739532b
TechyShie <susanshie316@gmail.com>
2025-06-19 20:50:00 +0300

    Complete functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+
+const mondayWork = function(activity = "go to the office"){
+  return `This Monday, I will ${activity}.`;
+};
+
+
+function wrapAdjective(wrapper = "*"){
+    return function(Shie = "special"){
+        return `You are ${wrapper}${Shie}${wrapper}!`;
+
+    };
+}
+
+
+
+
+
